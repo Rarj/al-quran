76eb584de94b47de42bc2b57a4fc5544cd7066f3
Rio Arjuna <rio.arjuna@cpssoft.com>
2020-03-20 17:42:34 +0700

    wip

--- data/src/main/java/rio/arj/data/di/DataModule.kt
+++ data/src/main/java/rio/arj/data/di/DataModule.kt
@@ -11,6 +11,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import rio.arj.data.BuildConfig.BASE_URL
 import rio.arj.data.NetworkService
 import rio.arj.data.network.NetworkInterceptor
 import rio.arj.data.network.RequestInterceptor
