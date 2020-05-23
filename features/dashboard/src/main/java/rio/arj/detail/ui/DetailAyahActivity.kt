c382b633cfc981c8ef134e38e2a7179598266785
Rio Arjuna <ri0arjuna@programmer.net>
2020-05-23 11:08:53 +0700

    - add function play murattal

--- features/dashboard/src/main/java/rio/arj/detail/ui/DetailAyahActivity.kt
+++ features/dashboard/src/main/java/rio/arj/detail/ui/DetailAyahActivity.kt
@@ -1,7 +1,12 @@
 package rio.arj.detail.ui
 
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -22,12 +27,16 @@ class DetailAyahActivity : AppCompatActivity() {
   @Inject
   lateinit var viewModelFactory: ViewModelProvider.Factory
 
-  lateinit var viewModel: DetailAyahViewModel
-  lateinit var binding: ActivityDetailAyahBinding
+  private lateinit var viewModel: DetailAyahViewModel
+  private lateinit var binding: ActivityDetailAyahBinding
 
-  lateinit var detailAyahAdapter: DetailAyahAdapter
-  val SURAH_ALFATIHAH = "1"
-  lateinit var data: Data
+  private val detailAyahAdapter: DetailAyahAdapter by lazy {
+    DetailAyahAdapter(viewModel.detailAyahModel.value!!)
+  }
+  private val SURAH_ALFATIHAH = "1"
+  private lateinit var data: Data
+
+  private var mediaPlayer: MediaPlayer? = null
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -44,6 +53,10 @@ class DetailAyahActivity : AppCompatActivity() {
     binding.dataModel = data
     binding.lifecycleOwner = this
 
+    mediaPlayer = MediaPlayer()
+    mediaPlayer?.setDataSource(this, Uri.parse(data.audio))
+    mediaPlayer?.prepareAsync()
+
     viewModel.loadDetailAyah(data.nomor.toString())
 
     listener()
@@ -52,6 +65,39 @@ class DetailAyahActivity : AppCompatActivity() {
 
   private fun listener() {
     binding.toolbarAyah.setNavigationOnClickListener { finish() }
+
+    var isPlayedMurottal = false
+    binding.toolbarAyah.setOnMenuItemClickListener { menuItem ->
+      when (menuItem.itemId) {
+        R.id.item_play_murottal -> {
+          mediaPlayer?.setOnCompletionListener { _ ->
+            isPlayedMurottal = false
+            setTitleMenuItem(menuItem, getString(R.string.detail_play_murattal), R.drawable.ic_play)
+          }
+
+          isPlayedMurottal = !isPlayedMurottal
+          if (isPlayedMurottal) {
+            mediaPlayer?.start()
+            setTitleMenuItem(menuItem, getString(R.string.detail_stop_murattal), R.drawable.ic_stop)
+          } else {
+            if (mediaPlayer?.isPlaying!!) {
+              mediaPlayer?.pause()
+              mediaPlayer?.seekTo(0)
+            }
+            setTitleMenuItem(menuItem, getString(R.string.detail_play_murattal), R.drawable.ic_play)
+          }
+        }
+        R.id.item_share_surah -> {
+
+        }
+      }
+      true
+    }
+  }
+
+  private fun setTitleMenuItem(menuItem: MenuItem, title: String, @DrawableRes drawable: Int) {
+    menuItem.title = title
+    menuItem.icon = ContextCompat.getDrawable(this, drawable)
   }
 
   private fun observer() {
@@ -59,7 +105,6 @@ class DetailAyahActivity : AppCompatActivity() {
       binding.isSuccess = listAyah.isNotEmpty()
       if (listAyah.isNotEmpty()) {
         removeBismillah(listAyah)
-        detailAyahAdapter = DetailAyahAdapter(listAyah)
         binding.recyclerAyah.apply {
           layoutManager = LinearLayoutManager(this@DetailAyahActivity)
           addItemDecoration(ItemDecoration(16))
@@ -81,6 +126,42 @@ class DetailAyahActivity : AppCompatActivity() {
               listAyah[0].id,
               listAyah[0].nomor, listAyah[0].tr
         )
+      }
+    }
+
+    viewModel.detailAyahModel.value?.add(0, DetailAyahModelItem(
+          getString(R.string.detail_bismillah_ar),
+          getString(R.string.detail_bismillah_mean),
+          "0",
+          getString(R.string.detail_bismillah_id)
+    ))
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+
+    if (mediaPlayer != null) {
+      mediaPlayer?.stop()
+      mediaPlayer?.release()
+    }
+  }
+
+  override fun onPause() {
+    super.onPause()
+
+    if (mediaPlayer != null) {
+      if (mediaPlayer!!.isPlaying) {
+        mediaPlayer!!.pause()
+      }
+    }
+  }
+
+  override fun onResume() {
+    super.onResume()
+
+    if (mediaPlayer != null) {
+      if (!mediaPlayer!!.isPlaying && mediaPlayer!!.currentPosition > 1) {
+        mediaPlayer?.start()
       }
     }
   }
